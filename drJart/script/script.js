d8ded7489892313602c83efc67cef639247e2081
rocio-git <148847927+rocio-git@users.noreply.github.com>
2023-11-27 00:51:00 +0900

    dropdown upload

--- drJart/script/script.js
+++ drJart/script/script.js
@@ -1,5 +1,16 @@
 $(function(){
 
+  //드롭다운 메뉴
+  // $(".dropdown-wrap").hide();
+  $(".gnb > li.has-dropdown").mouseover(function(){
+    $(this).find(".dropdown-wrap").stop().fadeIn(600);
+  });
+  $(".gnb > li.has-dropdown").mouseout(function(){
+    $(this).find(".dropdown-wrap").stop().fadeOut(600);
+  });
+
+
+
   //로딩바 : shopnow, our story
  $(".load-box").mouseover(function(){
   $(".loading-bar").stop().animate({width: 100 + "%"}, 600);
@@ -7,6 +18,7 @@ $(function(){
  $(".load-box").mouseout(function(){
   $(".loading-bar").stop().animate({width:0}, 600);
  });
+
 
 
 // 모바일
@@ -19,6 +31,7 @@ $(".icon-close").on("click", function(){
   $(".moblie__dropdown").stop().slideUp(100);
   $("html").css("overflow", "auto");
 });
+
 
 
 //모바일 서브메뉴

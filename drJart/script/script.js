1deb0005b652a022becbb73d375d9919ce10d63f
rocio-git <148847927+rocio-git@users.noreply.github.com>
2024-01-03 01:32:29 +0900

    upload

--- drJart/script/script.js
+++ drJart/script/script.js
@@ -58,6 +58,76 @@ $("a.item").each(function(){
 });
 
 
+//About : collections > li 넓이 조정
+
+ $(".select").css("width","85%").siblings().css("width","5%");
+
+  if ($(window).width() < 1280) {
+    $(".collections > ul > li").css("width","90%");
+    } else { //window 크기가 1280보다 클때
+      $(".select").css("width","85%").siblings().css("width","5%");
+      $(".collections > ul > li").on("click", function(){
+        $(this).stop().animate({width : 85 + "%"}, 500).siblings().stop().animate({width : 5 + "%"}, 400);
+      });
+    }
+
+    $(window).resize(function(){document.location.reload();})
+    // 리사이즈 끝나고 1초마다 리셋
+    const delay = 1000;
+    const timer = null; 
+    $(window).on('resize', function(){
+      clearTimeout(timer);
+      timer = setTimeout(function(){
+      document.location.reload();
+      }, delay);
+  });
+
+
+  //About : collections -right arrow
+  let currentIndex = 0;
+  $(".right-arrow").on("click", function(){
+    currentIndex++;
+    $(".collections > ul > li").eq(currentIndex).stop().animate({width : 85 + "%"}, 500).siblings().stop().animate({width : 5 + "%"}, 400);
+    $(".swiper > p").eq(currentIndex).css("background-color","#fff").siblings().css("background-color","#5f5f5f");
+  if(currentIndex == 4){
+    $(".collections > ul > li").eq(0).stop().animate({width : 85 + "%"}, 500).siblings().stop().animate({width : 5 + "%"}, 400);
+    $(".swiper > p").eq(0).css("background-color","#fff").siblings().css("background-color","#5f5f5f");
+    currentIndex = 0;
+    }
+  });
+
+
+  //About : collections -swiper 버튼
+  $(".swiper > p").on("click", function(){
+    let activeIndex = $(this).index();
+    $(".collections > ul > li").eq(activeIndex).stop().animate({width : 85 + "%"}, 500).siblings().stop().animate({width : 5 + "%"}, 400);
+    $(".swiper > p").eq(activeIndex).css("background-color","#fff").siblings().css("background-color","#5f5f5f");
+   
+    let currentIndex = activeIndex;
+    $(".right-arrow").on("click", function(){
+      currentIndex++;
+      $(".collections > ul > li").eq(currentIndex).stop().animate({width : 85 + "%"}, 500).siblings().stop().animate({width : 5 + "%"}, 400);
+      $(".swiper > p").eq(currentIndex).css("background-color","#fff").siblings().css("background-color","#5f5f5f");
+    if(currentIndex == 4){
+      $(".collections > ul > li").eq(0).stop().animate({width : 85 + "%"}, 500).siblings().stop().animate({width : 5 + "%"}, 400);
+      $(".swiper > p").eq(0).css("background-color","#fff").siblings().css("background-color","#5f5f5f");
+      currentIndex = 0;
+      }
+    });
+  });
+
+
+  //About : collections 로딩바
+  $(".about-load-box").mouseover(function(){
+    $(this).find(".about-loading-bar").stop().animate({width: 100 + "%"}, 600);
+   });
+  $(".about-load-box").mouseout(function(){
+    $(this).find(".about-loading-bar").stop().animate({width:0}, 600);
+   });
+
+
+
+
 
 
 

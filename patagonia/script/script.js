648b0a53e67c4207b180bb7d48a9b91ab00a9c3c
rocio-git <148847927+rocio-git@users.noreply.github.com>
2024-01-23 14:19:55 +0900

    upload

--- patagonia/script/script.js
+++ patagonia/script/script.js
@@ -12,6 +12,8 @@ $(function(){
   });
 
 
+
+  //main : img 스크립트
   $(".img-conts").mouseover(function(){
     $(this).toggleClass('on');
     $(this).find(".main-img-desc2").css("padding-left","30px");
@@ -22,5 +24,157 @@ $(function(){
     $(this).find(".main-img-desc2").css("padding-left","0");
     $(this).find(".icon-caret-right-fill").css("opacity","0");
   });
+
+
+
+  //section : slider-1 
+  let slide = $(".slider1-wrapper > div");
+  let slideCount = slide.length;
+  let currentIdx = 0;
+
+  slide.hide().eq(currentIdx).show();
+
+  setInterval(showNextSlide, 3400);
+
+  function showNextSlide(){
+    let nextIdx = (currentIdx + 1) % slideCount;
+    slide.eq(currentIdx).fadeOut();
+    slide.eq(nextIdx).fadeIn();
+    currentIdx = nextIdx;
+  }
+
+
+
+  //section : collection 스크립트
+  $(".collection_img_conts").mouseover(function(){
+    $(this).toggleClass('on');
+    $(this).find(".collection_img_desc2").css("padding-left","30px");
+    $(this).find(".icon-caret-right-fill").css("opacity","100");
+  });
+  $(".collection_img_conts").mouseout(function(){
+    $(this).removeClass('on');
+    $(this).find(".collection_img_desc2").css("padding-left","0");
+    $(this).find(".icon-caret-right-fill").css("opacity","0");
+  });
+
+
+
+  //section : shop
+  $('.shop_wrapper').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 3200,
+    arrows: true,
+    prevArrow : "<button type='button' class='slick-prev'></button>",
+    nextArrow : "<button type='button' class='slick-next'></button>",
+    pauseOnHover: false,
+    centerMode: true,
+    centerPadding: '44px',
+    responsive : [
+      {
+        breakpoint : 960,
+        settings : {
+          slidesToShow: 2,
+        }
+      }
+    ],
+    dots: true,
+    appendDots: ".controler",
+    customPaging: function(slider, i){
+      return '<p>' + '0' + (i + 1) + '</p><p>' + '0' + slider.slideCount + '</p>';
+    }
+  });
+
+
+
+  // section : anniver50 스크립트
+    $(".anniver50_img_conts").mouseover(function(){
+      $(this).toggleClass('on');
+      $(this).find(".anniver50_img_desc2").css("padding-left","30px");
+      $(this).find(".icon-caret-right-fill").css("opacity","100");
+    });
+    $(".anniver50_img_conts").mouseout(function(){
+      $(this).removeClass('on');
+      $(this).find(".anniver50_img_desc2").css("padding-left","0");
+      $(this).find(".icon-caret-right-fill").css("opacity","0");
+    });
+
+
+
+    // section : anniver50_items
+    $(".img_conts").mouseover(function(){
+      $(this).toggleClass('on');
+      $(this).find(".anniver50_items_img_desc2").css("padding-left","30px");
+      $(this).find(".icon-caret-right-fill").css("opacity","100");
+    });
+    $(".img_conts").mouseout(function(){
+      $(this).removeClass('on');
+      $(this).find(".anniver50_items_img_desc2").css("padding-left","0");
+      $(this).find(".icon-caret-right-fill").css("opacity","0");
+    });
+
+
+
+    //section : shop
+    $('.anniver50_shop_wrapper').slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: false,
+      autoplaySpeed: 3200,
+      arrows: true,
+      prevArrow : "<button type='button' class='slick-prev'></button>",
+      nextArrow : "<button type='button' class='slick-next'></button>",
+      pauseOnHover: false,
+      centerMode: true,
+      centerPadding: '44px',
+      responsive : [
+        {
+          breakpoint : 960,
+          settings : {
+            slidesToShow: 2,
+          }
+        }
+      ],
+      dots: true,
+      appendDots: ".anniver50_shop_controler",
+      customPaging: function(slider, i){
+        return '<p>' + '0' + (i + 1) + '</p><p>' + '0' + slider.slideCount + '</p>';
+      }
+    });
+
+
+
+  //section : slider-2 
+  let slider2slide = $(".slider2_wrapper > div");
+  let slider2Count = slider2slide.length;
+  let slider2currentIdx = 0;
+
+  slider2slide.hide().eq(slider2currentIdx).show();
+
+  setInterval(slider2NextSlide, 3500);
+
+  function slider2NextSlide(){
+    let slider2nextIdx = (slider2currentIdx + 1) % slider2Count;
+    slider2slide.eq(slider2currentIdx).fadeOut();
+    slider2slide.eq(slider2nextIdx).fadeIn();
+    slider2currentIdx = slider2nextIdx;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 });

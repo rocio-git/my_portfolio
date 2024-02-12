ddaa116035d9ee22099b484c5bfe10e4200c2233
rocio-git <148847927+rocio-git@users.noreply.github.com>
2024-02-13 00:03:41 +0900

    upload

---
+++ patagonia/script/chemistry.js
@@ -0,0 +1,18 @@
+$(function(){
+
+
+
+  //section : goretex_ski 스크립트
+  
+  $(".goretex_ski_img_conts").mouseover(function(){
+    $(this).toggleClass('on');
+    $(this).find(".goretex_ski_img_desc2").css("padding-left","20px");
+    $(this).find(".icon-caret-right-fill").css("opacity","100");
+  });
+  $(".goretex_ski_img_conts").mouseout(function(){
+    $(this).removeClass('on');
+    $(this).find(".goretex_ski_img_desc2").css("padding-left","0");
+    $(this).find(".icon-caret-right-fill").css("opacity","0");
+  });
+
+});

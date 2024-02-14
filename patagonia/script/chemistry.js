40d16542f7e895425c2f13d5c2c85c54153649ce
rocio-git <148847927+rocio-git@users.noreply.github.com>
2024-02-14 11:33:57 +0900

    upload

--- patagonia/script/chemistry.js
+++ patagonia/script/chemistry.js
@@ -15,4 +15,31 @@ $(function(){
     $(this).find(".icon-caret-right-fill").css("opacity","0");
   });
 
+
+
+    //section : goretex_climbimg 스크립트
+  
+    $(".goretex_climbing_img_conts").mouseover(function(){
+      $(this).toggleClass('on');
+      $(this).find(".goretex_climbing_img_desc2").css("padding-left","20px");
+      $(this).find(".icon-caret-right-fill").css("opacity","100");
+    });
+    $(".goretex_climbing_img_conts").mouseout(function(){
+      $(this).removeClass('on');
+      $(this).find(".goretex_climbing_img_desc2").css("padding-left","0");
+      $(this).find(".icon-caret-right-fill").css("opacity","0");
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
 });

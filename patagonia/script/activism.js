a3b06aa2252be276516d6dcb2ff6c02379afdb4e
rocio-git <148847927+rocio-git@users.noreply.github.com>
2024-02-17 00:09:29 +0900

    progressBar 제작

---
+++ patagonia/script/activism.js
@@ -0,0 +1,35 @@
+$(function(){
+
+
+  //section : effort > slide_progress_bar
+
+  let showIdx = $(".effort_desc > ul > li .desc_info");
+  let idxCount = showIdx.length;
+  let firstIdx = 0;
+  let progress = $(".slide_progress_bar");
+
+  showIdx.hide().eq(firstIdx).show();
+  progress.hide().eq(firstIdx).show();
+
+  setInterval(showNextIndex, 4000);
+
+  function showNextIndex(){
+    let nextIndex = (firstIdx + 1) % idxCount;
+    showIdx.eq(firstIdx).hide(300);
+    showIdx.eq(nextIndex).show(300);
+    progress.eq(firstIdx).hide();
+    progress.eq(nextIndex).show();
+    firstIdx = nextIndex;
+  }
+
+
+
+
+
+
+
+
+
+
+
+});

cb392550d7dbf09e9f979d2d28d36a06fa5d94b6
rocio-git <148847927+rocio-git@users.noreply.github.com>
2024-03-20 03:22:27 +0900

    JS-ArrowUp

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 //Home 섹션을 아래로 스크롤시 투명하게 처리함
 const home = document.querySelector('.home__container');
-const homeheight = home.offsetHeight;
+const homeHeight = home.offsetHeight;
 document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeheight;
+});
+
+// Arrow-up버튼을 아래로 스크롤 시 투명하게 처리함
+const arrowUp = document.querySelector('.arrow-up');
+arrowUp.style.opacity = 0;
+document.addEventListener('scroll', () => {
+if( window.scrollY > homeHeight / 2) {
+arrowUp.style.opacity = 1;
+} else {
+  arrowUp.style.opacity = 0;
+}
 });

ac97c8ba4de762242bdcbffca31c7f449bdbbd8f
rocio-git <148847927+rocio-git@users.noreply.github.com>
2024-03-20 03:09:54 +0900

    JS-Home

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
-
+//Home 섹션을 아래로 스크롤시 투명하게 처리함
+const home = document.querySelector('.home__container');
+const homeheight = home.offsetHeight;
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeheight;
+});

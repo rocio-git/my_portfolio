f3544363c803f234d2f75e3001c6f88ea67e8eaf
rocio-git <148847927+rocio-git@users.noreply.github.com>
2024-03-22 16:28:15 +0900

    JS-Menu

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeheight;
 });
 
+
+
 // Arrow-up버튼을 아래로 스크롤 시 투명하게 처리함
 const arrowUp = document.querySelector('.arrow-up');
 arrowUp.style.opacity = 0;
@@ -14,4 +16,21 @@ arrowUp.style.opacity = 1;
 } else {
   arrowUp.style.opacity = 0;
 }
+});
+
+
+
+//Navbar 토글버튼 클릭 처리
+const navBarMenu = document.querySelector('.header__menu');
+const navBarToggle = document.querySelector('.header__toggle');
+
+navBarToggle.addEventListener('click', () => {
+  navBarMenu.classList.toggle('open');
+});
+
+
+
+//Navbar 메뉴 클릭시 메뉴 자동 닫기
+navBarMenu.addEventListener('click', () => {
+  navBarMenu.classList.remove('open');
 });
